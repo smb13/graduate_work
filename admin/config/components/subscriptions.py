dee5a7b6f958d759896f10f45e34da5050abb4bf
Федор Зоркий <fedor.zorky@nanosemantics.ai>
2024-04-25 01:14:04 +0300

    Доработка докера.

--- admin/config/components/subscriptions.py
+++ admin/config/components/subscriptions.py
@@ -1,6 +1,6 @@
 from config.components.base import env
 
-SUBSCRIPTIONS_SERVICE_HOST = env("SUBSCRIPTIONS_SERVICE_HOST", cast=str, default="subscriptions")
-SUBSCRIPTIONS_SERVICE_PORT = env("SUBSCRIPTIONS_SERVICE_PORT", cast=str, default="8000")
+SUBSCRIPTIONS_SERVICE_HOST = env("SUBSCRIPTION_SERVICE_HOST", cast=str, default="subscriptions")
+SUBSCRIPTIONS_SERVICE_PORT = env("SUBSCRIPTION_SERVICE_PORT", cast=str, default="8000")
 
 SUBSCRIPTIONS_LIST_PER_PAGE = 100
